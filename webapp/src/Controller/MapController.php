00f9138189fe2a6bb2406944a2a5d5a93a1016ce
QuentinLReinette <185085787+QuentinLReinette@users.noreply.github.com>
2026-03-30 16:38:07 +0200

    add map view to home page

---
+++ webapp/src/Controller/MapController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Map\Bridge\Leaflet\LeafletOptions;
+use Symfony\UX\Map\Bridge\Leaflet\Option\TileLayer;
+use Symfony\UX\Map\InfoWindow;
+use Symfony\UX\Map\Map;
+use Symfony\UX\Map\Marker;
+use Symfony\UX\Map\Point;
+
+class MapController extends AbstractController
+{
+    #[Route('/', name: 'app_map')]
+    public function __invoke(): Response
+    {
+        $map = (new Map('default'))
+            ->center(new Point(45.7534031, 4.8295061))
+            ->zoom(6)
+            ->addMarker(new Marker(
+                position: new Point(45.7534031, 4.8295061),
+                title: 'Lyon',
+                infoWindow: new InfoWindow(
+                    content: '<p>Thank you <a href="https://github.com/Kocal">@Kocal</a> for this component!</p>',
+                )
+            ))
+            ->options((new LeafletOptions())
+                ->tileLayer(new TileLayer(
+                    url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                    options: ['maxZoom' => 19]
+                ))
+            );
+
+        return $this->render('map.html.twig', [
+            'map' => $map,
+        ]);
+    }
+}
