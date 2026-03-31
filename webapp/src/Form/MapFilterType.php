e67b86601f4011a3211e932f8ba8015776423fbd
titiThibaud1 <thibaud.sellier@ynov.com>
2026-03-31 10:37:59 +0200

    forms

--- webapp/src/Form/MapFilterType.php
+++ webapp/src/Form/MapFilterType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class MapFilterType extends AbstractType
@@ -11,8 +13,28 @@ class MapFilterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date', DateTimeType::class, [
-                'label' => 'Date',
+            ->add('dateMin', DateTimeType::class, [
+                'label' => 'Date minimum',
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('dateMax', DateTimeType::class, [
+                'label' => 'Date maximum',
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('indexMin', NumberType::class, [
+                'label' => 'Score minimum',
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('indexMax', NumberType::class, [
+                'label' => 'Score maximum',
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('searchArea', TextType::class, [
+                'label' => 'Zone de recherche ',
                 'widget' => 'single_text',
                 'required' => false,
             ])
--- webapp/src/Model/MapFilterData.php
+++ webapp/src/Model/MapFilterData.php
@@ -4,16 +4,68 @@ namespace App\Model;
 
 class MapFilterData
 {
-    public ?\DateTime $date = null;
+    public ?\DateTime $dateMin = null;
+    public ?\DateTime $dateMax = null;
+    public ?float $indexMin = null;
+    public ?float $indexMax = null;
+    public ?string $searchArea = null;
 
-    public function getDate(): ?\DateTime
+    public function getDateMin(): ?\DateTime
     {
-        return $this->date;
+        return $this->dateMin;
     }
 
-    public function setDate(?\DateTime $date): self
+    public function setDateMin(?\DateTime $dateMin): self
     {
-        $this->date = $date;
+        $this->dateMin = $dateMin;
+
+        return $this;
+    }
+
+    public function getDateMax(): ?\DateTime
+    {
+        return $this->dateMax;
+    }
+
+    public function setDateMax(?\DateTime $dateMax): self
+    {
+        $this->dateMax = $dateMax;
+
+        return $this;
+    }
+
+    public function getIndexMin(): ?float
+    {
+        return $this->indexMin;
+    }
+
+    public function setIndexMin(?float $indexMin): self
+    {
+        $this->indexMin = $indexMin;
+
+        return $this;
+    }
+
+    public function getIndexMax(): ?float
+    {
+        return $this->indexMax;
+    }
+
+    public function setIndexMax(?float $indexMax): self
+    {
+        $this->indexMax = $indexMax;
+
+        return $this;
+    }
+
+    public function getSearchArea(): ?string
+    {
+        return $this->searchArea;
+    }
+
+    public function setSearchArea(?string $searchArea): self
+    {
+        $this->searchArea = $searchArea;
 
         return $this;
     }
